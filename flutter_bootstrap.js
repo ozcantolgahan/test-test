6517638d0475e58186276835efa1dd9979668ad2
ozcantolgahan <tolgahanozcan@hotmail.com>
2024-09-04 11:02:40 +0300

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "284541694"
+    serviceWorkerVersion: "1388753738"
   }
 });
